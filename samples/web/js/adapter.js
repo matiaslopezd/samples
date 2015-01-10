a5d98c820ec1811ec4c308ee82f10b4bdea6b9d0
Philipp Hancke <fippo@andyet.net>
2015-01-10 14:59:47 +0100

    variant 2

--- samples/web/js/adapter.js
+++ samples/web/js/adapter.js
@@ -19,6 +19,7 @@ var attachMediaStream = null;
 var reattachMediaStream = null;
 var webrtcDetectedBrowser = null;
 var webrtcDetectedVersion = null;
+var requestUserMedia = null;
 
 function trace(text) {
   // This function is used for logging.
@@ -33,15 +34,157 @@ function trace(text) {
   }
 }
 
-function maybeFixConfiguration(pcConfig) {
-  if (!pcConfig) {
-    return;
+if (navigator.mozGetUserMedia || navigator.webkitGetUserMedia) {
+  // getUserMedia shim (only difference is the prefix).
+  if (navigator.mozGetUserMedia) {
+    // Code from Adam Barth.
+    getUserMedia = navigator.mozGetUserMedia.bind(navigator);
+    navigator.getUserMedia = getUserMedia;
+
+    // Shim for MediaStreamTrack.getSources.
+    MediaStreamTrack.getSources = function(successCb) {
+      setTimeout(function() {
+        var infos = [
+          { kind: 'audio', id: 'default', label:'', facing:'' },
+          { kind: 'video', id: 'default', label:'', facing:'' }
+        ];
+        successCb(infos);
+      }, 0);
+    };
+  } else {
+    // Code from Adam Barth.
+    getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
+    navigator.getUserMedia = getUserMedia;
   }
-  for (var i = 0; i < pcConfig.iceServers.length; i++) {
-    if (pcConfig.iceServers[i].hasOwnProperty('urls')) {
-      pcConfig.iceServers[i].url = pcConfig.iceServers[i].urls;
-      delete pcConfig.iceServers[i].urls;
-    }
+  // Returns the result of getUserMedia as a Promise.
+  requestUserMedia = function (constraints) {
+    return new Promise(function(resolve, reject) {
+      var onSuccess = function(stream) {
+        resolve(stream);
+      };
+      var onError = function(error) {
+        reject(error);
+      };
+
+      try {
+        getUserMedia(constraints, onSuccess, onError);
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+}
+
+
+if (mozRTCPeerConnection || webkitRTCPeerConnection) {
+  if (mozRTCPeerConnection) {
+    // The RTCPeerConnection object.
+    RTCPeerConnection = function(pcConfig, pcConstraints) {
+      // .urls is not supported in FF yet.
+      maybeFixConfiguration = function (pcConfig) {
+        if (!pcConfig) {
+          return;
+        }
+        for (var i = 0; i < pcConfig.iceServers.length; i++) {
+          if (pcConfig.iceServers[i].hasOwnProperty('urls')) {
+            pcConfig.iceServers[i].url = pcConfig.iceServers[i].urls;
+            delete pcConfig.iceServers[i].urls;
+          }
+        }
+      }
+      maybeFixConfiguration(pcConfig);
+      return new mozRTCPeerConnection(pcConfig, pcConstraints);
+    };
+
+    // The RTCSessionDescription object.
+    window.RTCSessionDescription = mozRTCSessionDescription;
+
+    // The RTCIceCandidate object.
+    window.RTCIceCandidate = mozRTCIceCandidate;
+
+    // Creates ICE server from the URL for FF.
+    window.createIceServer = function(url, username, password) {
+      var iceServer = null;
+      var urlParts = url.split(':');
+      if (urlParts[0].indexOf('stun') === 0) {
+        // Create ICE server with STUN URL.
+        iceServer = {
+          'url': url
+        };
+      } else if (urlParts[0].indexOf('turn') === 0) {
+        if (webrtcDetectedVersion < 27) {
+          // Create iceServer with turn url.
+          // Ignore the transport parameter from TURN url for FF version <=27.
+          var turnUrlParts = url.split('?');
+          // Return null for createIceServer if transport=tcp.
+          if (turnUrlParts.length === 1 ||
+            turnUrlParts[1].indexOf('transport=udp') === 0) {
+            iceServer = {
+              'url': turnUrlParts[0],
+              'credential': password,
+              'username': username
+            };
+          }
+        } else {
+          // FF 27 and above supports transport parameters in TURN url,
+          // So passing in the full url to create iceServer.
+          iceServer = {
+            'url': url,
+            'credential': password,
+            'username': username
+          };
+        }
+      }
+      return iceServer;
+    };
+
+    window.createIceServers = function(urls, username, password) {
+      var iceServers = [];
+      // Use .url for FireFox.
+      for (var i = 0; i < urls.length; i++) {
+        var iceServer =
+          window.createIceServer(urls[i], username, password);
+        if (iceServer !== null) {
+          iceServers.push(iceServer);
+        }
+      }
+      return iceServers;
+    };
+
+  } else {
+    // Creates iceServer from the url for Chrome M33 and earlier.
+    window.createIceServer = function(url, username, password) {
+      var iceServer = null;
+      var urlParts = url.split(':');
+      if (urlParts[0].indexOf('stun') === 0) {
+        // Create iceServer with stun url.
+        iceServer = {
+          'url': url
+        };
+      } else if (urlParts[0].indexOf('turn') === 0) {
+        // Chrome M28 & above uses below TURN format.
+        iceServer = {
+          'url': url,
+          'credential': password,
+          'username': username
+        };
+      }
+      return iceServer;
+    };
+
+    // Creates an ICEServer object from multiple URLs.
+    window.createIceServers = function(urls, username, password) {
+      return {
+        'urls': urls,
+        'credential': password,
+        'username': username
+      };
+    };
+
+    // The RTCPeerConnection object.
+    RTCPeerConnection = function(pcConfig, pcConstraints) {
+      return new webkitRTCPeerConnection(pcConfig, pcConstraints);
+    };
   }
 }
 
@@ -52,84 +195,6 @@ if (navigator.mozGetUserMedia && mozRTCPeerConnection) {
 
   webrtcDetectedVersion =
     parseInt(navigator.userAgent.match(/Firefox\/([0-9]+)\./)[1], 10);
-
-  // The RTCPeerConnection object.
-  RTCPeerConnection = function(pcConfig, pcConstraints) {
-    // .urls is not supported in FF yet.
-    maybeFixConfiguration(pcConfig);
-    return new mozRTCPeerConnection(pcConfig, pcConstraints);
-  };
-
-  // The RTCSessionDescription object.
-  window.RTCSessionDescription = mozRTCSessionDescription;
-
-  // The RTCIceCandidate object.
-  window.RTCIceCandidate = mozRTCIceCandidate;
-
-  // getUserMedia shim (only difference is the prefix).
-  // Code from Adam Barth.
-  getUserMedia = navigator.mozGetUserMedia.bind(navigator);
-  navigator.getUserMedia = getUserMedia;
-
-  // Shim for MediaStreamTrack.getSources.
-  MediaStreamTrack.getSources = function(successCb) {
-    setTimeout(function() {
-      var infos = [
-        { kind: 'audio', id: 'default', label:'', facing:'' },
-        { kind: 'video', id: 'default', label:'', facing:'' }
-      ];
-      successCb(infos);
-    }, 0);
-  };
-
-  // Creates ICE server from the URL for FF.
-  window.createIceServer = function(url, username, password) {
-    var iceServer = null;
-    var urlParts = url.split(':');
-    if (urlParts[0].indexOf('stun') === 0) {
-      // Create ICE server with STUN URL.
-      iceServer = {
-        'url': url
-      };
-    } else if (urlParts[0].indexOf('turn') === 0) {
-      if (webrtcDetectedVersion < 27) {
-        // Create iceServer with turn url.
-        // Ignore the transport parameter from TURN url for FF version <=27.
-        var turnUrlParts = url.split('?');
-        // Return null for createIceServer if transport=tcp.
-        if (turnUrlParts.length === 1 ||
-          turnUrlParts[1].indexOf('transport=udp') === 0) {
-          iceServer = {
-            'url': turnUrlParts[0],
-            'credential': password,
-            'username': username
-          };
-        }
-      } else {
-        // FF 27 and above supports transport parameters in TURN url,
-        // So passing in the full url to create iceServer.
-        iceServer = {
-          'url': url,
-          'credential': password,
-          'username': username
-        };
-      }
-    }
-    return iceServer;
-  };
-
-  window.createIceServers = function(urls, username, password) {
-    var iceServers = [];
-    // Use .url for FireFox.
-    for (var i = 0; i < urls.length; i++) {
-      var iceServer =
-        window.createIceServer(urls[i], username, password);
-      if (iceServer !== null) {
-        iceServers.push(iceServer);
-      }
-    }
-    return iceServers;
-  };
 
   // Attach a media stream to an element.
   attachMediaStream = function(element, stream) {
@@ -155,40 +220,6 @@ if (navigator.mozGetUserMedia && mozRTCPeerConnection) {
     webrtcDetectedVersion = 999;
   }
 
-  // Creates iceServer from the url for Chrome M33 and earlier.
-  window.createIceServer = function(url, username, password) {
-    var iceServer = null;
-    var urlParts = url.split(':');
-    if (urlParts[0].indexOf('stun') === 0) {
-      // Create iceServer with stun url.
-      iceServer = {
-        'url': url
-      };
-    } else if (urlParts[0].indexOf('turn') === 0) {
-      // Chrome M28 & above uses below TURN format.
-      iceServer = {
-        'url': url,
-        'credential': password,
-        'username': username
-      };
-    }
-    return iceServer;
-  };
-
-  // Creates an ICEServer object from multiple URLs.
-  window.createIceServers = function(urls, username, password) {
-    return {
-      'urls': urls,
-      'credential': password,
-      'username': username
-    };
-  };
-
-  // The RTCPeerConnection object.
-  RTCPeerConnection = function(pcConfig, pcConstraints) {
-    return new webkitRTCPeerConnection(pcConfig, pcConstraints);
-  };
-
   // Get UserMedia (only difference is the prefix).
   // Code from Adam Barth.
   getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
@@ -212,23 +243,5 @@ if (navigator.mozGetUserMedia && mozRTCPeerConnection) {
   };
 } else {
   console.log('Browser does not appear to be WebRTC-capable');
-}
-
-// Returns the result of getUserMedia as a Promise.
-function requestUserMedia(constraints) {
-  return new Promise(function(resolve, reject) {
-    var onSuccess = function(stream) {
-      resolve(stream);
-    };
-    var onError = function(error) {
-      reject(error);
-    };
-
-    try {
-      getUserMedia(constraints, onSuccess, onError);
-    } catch (e) {
-      reject(e);
-    }
-  });
 }
 
