23242c3593e95d61919ef626b033cc46bf7cea36
Philipp Hancke <fippo@andyet.net>
2015-04-27 16:34:42 -0700

    also shift it

--- src/content/peerconnection/trickle-ice/js/main.js
+++ src/content/peerconnection/trickle-ice/js/main.js
@@ -166,7 +166,7 @@ function iceCallback(event) {
         // this should always be the case but...
         var old = cands.map(function (cand) { return cand.foundation + ':' + cand.component; });
         var idx = old.indexOf(c.foundation + ':' + c.component);
-        if (idx > -1 && ((c.priority >> 24) < (cands[idx].priority))) {
+        if (idx > -1 && ((c.priority >> 24) < (cands[idx].priority >> 24))) {
             console.log('dropped', c);
             return;
         }
