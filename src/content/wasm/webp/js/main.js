bfd464b07ab17a0024e0f460e92c05594bdf122b
Philipp Hancke <philipp@lifeonair.com>
2019-01-27 07:52:05 -0800

    webrtc/webp comparison

--- src/content/wasm/webp/js/main.js
+++ src/content/wasm/webp/js/main.js
@@ -2,40 +2,43 @@
 const localContext = localCanvas.getContext('2d');
 const remoteContext = remoteCanvas.getContext('2d');
 
-const pc1 = new RTCPeerConnection();
-const pc2 = new RTCPeerConnection();
-pc1.onicecandidate = e => pc2.addIceCandidate(e.candidate);
-pc2.onicecandidate = e => pc1.addIceCandidate(e.candidate);
-const sendChannel = pc1.createDataChannel('sendDataChannel');
-let receiveChannel;
-const recvBuffer = [];
-pc2.ondatachannel = e => {
-  receiveChannel = e.channel;
-  receiveChannel.onmessage = (ev) => {
-    const encoded = new Uint8Array(ev.data);
-    const {data, width, height} = libwebp.decode(encoded);
-    const frame = remoteContext.createImageData(width, height);
-    frame.data.set(data, 0);
-    remoteContext.putImageData(frame, 0, 0);
-  };
-};
-
-pc1.createOffer()
-  .then(offer => {
-    return pc2.setRemoteDescription(offer)
-        .then(() => pc1.setLocalDescription(offer));
-  })
-  .then(() => pc2.createAnswer())
-  .then(answer => {
-    return pc1.setRemoteDescription(answer)
-        .then(() => pc2.setLocalDescription(answer));
-  })
-  .catch(e => console.error(e));
-
 const libwebp = new LibWebP();
 navigator.mediaDevices.getUserMedia({video: {width: 320, height: 240}})
   .then(stream => {
+    const pc1 = new RTCPeerConnection();
+    const pc2 = new RTCPeerConnection();
+    pc1.onicecandidate = e => pc2.addIceCandidate(e.candidate);
+    pc2.onicecandidate = e => pc1.addIceCandidate(e.candidate);
+    const sendChannel = pc1.createDataChannel('sendDataChannel');
+    pc1.addTrack(stream.getTracks()[0], stream);
+    let receiveChannel;
+    const recvBuffer = [];
+    pc2.ondatachannel = e => {
+      receiveChannel = e.channel;
+      receiveChannel.onmessage = (ev) => {
+        const encoded = new Uint8Array(ev.data);
+        const {data, width, height} = libwebp.decode(encoded);
+        const frame = remoteContext.createImageData(width, height);
+        frame.data.set(data, 0);
+        remoteContext.putImageData(frame, 0, 0);
+      };
+    };
+    pc2.ontrack = (e) => remoteVideo2.srcObject = e.streams[0];
+    pc1.createOffer()
+      .then(offer => {
+        return pc2.setRemoteDescription(offer)
+            .then(() => pc1.setLocalDescription(offer));
+      })
+      .then(() => pc2.createAnswer())
+      .then(answer => {
+        return pc1.setRemoteDescription(answer)
+            .then(() => pc2.setLocalDescription(answer));
+      })
+      .catch(e => console.error(e));
+
+
     localVideo.srcObject = stream;
+    localVideo2.srcObject = stream;
     const width = 320;
     const height = 240;
 
@@ -45,19 +48,23 @@ navigator.mediaDevices.getUserMedia({video: {width: 320, height: 240}})
     remoteCanvas.height = height;
 
     let bytesSent = 0;
+    let frames = 0;
     let lastTime = Date.now();
-    const fps = 15;
+    const fps = 30;
     setInterval(() => {
         localContext.drawImage(localVideo, 0, 0, width, height);
         const frame = localContext.getImageData(0, 0, width, height);
         const encoded = libwebp.encode(frame);
         sendChannel.send(encoded); // 320x240 fits a single 65k frame.
         bytesSent += encoded.length;
+        frames++;
     }, 1000.0 / fps);
     setInterval(() => {
         const now = Date.now();
-        console.log('bitrate', 8.0 * bytesSent / (now - lastTime));
+        console.log('bitrate', Math.floor(8000.0 * bytesSent / (now - lastTime)),
+            'fps', Math.floor(1000.0 * frames / (now - lastTime)));
         bytesSent = 0;
+        frames = 0;
         lastTime = now;
     }, 1000);
 });
